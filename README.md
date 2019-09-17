d3923c9e9a76cd9df58914ee1492fa4f505f0d77
Balázs Sipos <sipblai@gmail.com>
2019-09-17 17:53:46 +0200

    Feature Gui
    image loading, replacing in gui
    test images

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rock-Paper-Scissors-Recognizer
+    Needed packages:
+        - opencv-python
+        - pillow
+        - appJar
---
+++ gui.py
@@ -0,0 +1,35 @@
+import cv2
+from PIL import Image, ImageTk
+from appJar import gui
+import numpy as np
+
+
+def opencv_image_to_appjar_image(image):
+    b, g, r = cv2.split(image)
+    im = Image.fromarray(cv2.merge((r, g, b)))
+    return ImageTk.PhotoImage(im)
+
+
+def submit(btn):
+    file_path = app.getEntry("f1")
+    if file_path != "":
+        img = cv2.imread(file_path)
+        app.reloadImageData("pic", opencv_image_to_appjar_image(img), fmt="PhotoImage")
+
+
+app = gui("RPS Recognizer")
+app.setStretch("none")
+app.addLabel("Rock-Paper-Scissors Recognizer")
+
+app.addFileEntry("f1")
+app.addButton("Submit", submit)
+
+#  default image
+image = cv2.imread("images/testing/tree.jpg")
+im = Image.fromarray(image)
+imtk = ImageTk.PhotoImage(im)
+
+app.addImageData("pic", imtk, fmt="PhotoImage")
+
+
+app.go()
